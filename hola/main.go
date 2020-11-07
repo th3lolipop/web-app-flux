8add3d3bf4b92b375e64aa91161090d263315e70
Ye Min <yemin.phyo@ascendcorp.com>
2020-11-07 18:45:48 +0630

    fix: ymp-fix2

--- hola/main.go
+++ hola/main.go
@@ -39,7 +39,7 @@ func main() {
 	tmpl := template.Must(template.ParseFiles("./index.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := InfoPageData{
-			PageTitle: "Welcome! WAIYANMIN, PHYOMINHTUN",
+			PageTitle: "Welcome! WAIYANMIN, PHYOMINHTUN, YEMINPHYO",
 			Infos: []Info{
 				{Title: "Pod Name - ", Done: hostName},
 				{Title: "Host - ", Done: r.Host},
